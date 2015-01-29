6f9c275080299bd5ab48c05d6b90b096bf16b419
suresh07 <soori_rozee@yahoo.com>
2015-01-29 15:28:45 +0530

    Commit No.3 Character set changed to UTF-8:

--- generate.sh
+++ generate.sh
@@ -5,7 +5,7 @@ db="sandesha"
 usr="root"
 pwd="mysql"
 
-echo "drop database sandesha; create database sandesha;" | /usr/bin/mysql -uroot -pmysql
+echo "drop database if exists sandesha; create database sandesha DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;" | /usr/bin/mysql -uroot -pmysql
 
 perl author.pl $host $db $usr $pwd
 perl feature.pl $host $db $usr $pwd
