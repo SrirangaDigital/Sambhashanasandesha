0ec2c1022cfc0df7bbafb276c97ac75a97c500dd
suresh07 <soori_rozee@yahoo.com>
2015-04-02 19:52:15 +0530

    Commit No. 72 - pointed to issue Feb 2015

--- php/connect.php
+++ php/connect.php
@@ -3,8 +3,8 @@ $user='root';
 $password='mysql';
 $database='sandesha';
 $server = 'localhost';
-$year = "2014";
-$month = "10";
+$year = "2015";
+$month = "02";
 $volume = "020";
-$issue = "01";
+$issue = "04";
 ?>
