761e22674b96133fba1376512dcfb86893ca6674
suresh07 <soori_rozee@yahoo.com>
2015-04-23 18:08:36 +0530

    Commit No. 89 - Php mailer implemented

--- php/sendmail.php
+++ php/sendmail.php
@@ -8,29 +8,45 @@
 	<section class="wrapper style4 special container 75%">
 		<div class="content">
 <?php
+require_once 'mail/class.phpmailer.php';
+require_once 'mail/class.pop3.php';
+require_once 'mail/class.smtp.php';
+require_once 'mail/PHPMailerAutoload.php';
+
 //if "email" variable is filled out, send email
-$name = $_POST['name'];
-$email = $_POST['email'];
+$fromName = $_POST['name'];
+$fromEmail = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
-if($name == '')
+if($fromName == '')
 {
 	echo "Name is required <br>";
 }
-elseif (!filter_var($email, FILTER_VALIDATE_EMAIL))
+elseif (!filter_var($fromEmail, FILTER_VALIDATE_EMAIL))
 {
 	echo "Invalid email format <br>";
 	
 }
 else
 {
-	$admin_email = "samskritam@gmail.com";
-	$headers = "From: $email" . "\n";
-	//send email
-	mail($admin_email, $subject, $message, $headers);
-  
-	//Email response
-	echo "Thank you for contacting us!";  
+	$toEmail = "arjun.kashyap@srirangadigital.com";
+	$toName = "Sriranga";
+	$mail = new PHPMailer();
+	$mail->isSendmail();
+	$mail->isHTML(true);
+	$mail->setFrom($fromEmail, $fromName);
+	$mail->addReplyTo($fromEmail, $fromName);
+	$mail->addAddress($toEmail, $toName);
+	$mail->Subject = $subject;
+	$mail->Body = $message;
+	$mail->SMTPDebug = 2;
+	
+	if(!$mail->send()) {
+		echo 'Message could not be sent.';
+		echo 'Mailer Error: ' . $mail->ErrorInfo;
+	} else {
+		echo 'Message has been sent';
+	}
 }
 ?>
 			
