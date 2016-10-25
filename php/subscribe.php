ef2942146853c4de98c23de39baa88eb761dc89a
shivasdst <shivably04sdst@gmail.com>
2016-10-25 10:54:55 +0530

    Changed subscription details

--- php/subscribe.php
+++ php/subscribe.php
@@ -13,18 +13,19 @@
 						<h3>Subscription to <strong>Sambhashana Sandesha</strong> in India</h3>
 					</header>
 					<ul>
-						<li>For 3 years (36 issues) Rs.300/- with a saving of Rs.60/-</li> 
-					   <li>For 2 years (24 issues) Rs.210/- with a saving of Rs.30/-</li>
-					   <li>For 1 year (12 issues) Rs.110/- with a saving of Rs.10/-</li>
+						<li>For 5 years (60 issues) Rs. 950/- </li> 
+					   <li>For 3 years (36 issues) Rs. 550/- </li>
+					   <li>For 1 year (12 issues) Rs. 200/- </li>
 					</ul>
-						<p>The amount may be sent by MO / DD / at par cheque drawn in favour of 'Sambhashana Sandeshah' to the following address: Sambhashana Sandesha<br /><br />
+						<p>The amount may be sent by MO / DD / at par cheque drawn in favour of 'Sambhashana Sandeshah' to the following address:<br /><br /> 
+						Sambhashana Sandesha<br />
 						"Aksharam"<br />
 						8th cross,<br />
-						Girinagar- 2nd phase<br />
-						Bangalore- 560 085<br />
-						Tel: [080]- 2672 1052 / 2672 2576 <br />
+						Girinagar - 2nd phase<br />
+						Bangalore - 560 085<br />
+						Tel: [080] - 2672 1052 / 2672 2576 <br />
 						e-mail: samskritam@gmail.com<br /><br />
-						<strong>Subscription amount may be sent through D.D/M.O./ e-transfer. Bank details for e-transfer</strong><br /><br />
+						<strong>Subscription amount may be sent through D.D/ M.O. / e-transfer. Bank details for e-transfer</strong><br /><br />
 						Name of the account - <strong>Sambhashana Sandesha</strong><br /> 
 						Bank details – Corporation Bank, Girinagar, Bangalore<br />
 						Account Type - <strong>CLSB</strong><br />
